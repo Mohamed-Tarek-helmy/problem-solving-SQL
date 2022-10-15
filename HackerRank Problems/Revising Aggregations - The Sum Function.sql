745ca0e4f41f5f57134f1f7e73fc69d78a5b3b81
Mohamed-Tarek-helmy <115006636+Mohamed-Tarek-helmy@users.noreply.github.com>
2022-10-16 00:27:21 +0200

    Create Revising Aggregations - The Sum Function.sql

---
+++ HackerRank Problems/Revising Aggregations - The Sum Function.sql
@@ -0,0 +1,14 @@
+/*
+
+Query the total population of all cities in CITY where District is California. 
+
+*/
+
+
+select sum (population) from city 
+group by  district
+having district ="California"
+
+
+
+
