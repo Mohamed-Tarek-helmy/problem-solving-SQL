77f6fcae62f7ceadc021db1aed778afa53b79d89
Mohamed-Tarek-helmy <115006636+Mohamed-Tarek-helmy@users.noreply.github.com>
2022-10-15 23:25:58 +0200

    Update do not start with vowels and do not end with vowels.sql

--- HackerRank Problems/do not start with vowels and do not end with vowels.sql
+++ HackerRank Problems/do not start with vowels and do not end with vowels.sql
@@ -1,5 +1,5 @@
 /*
-
+Weather Observation Station 12
 Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
 
 */
@@ -8,3 +8,8 @@ select DISTINCT CITY FROM STATION WHERE
 CITY NOT LIKE '[aoeiu]%'
 and
 CITY NOT LIKE '%[aoeiu]'
+
+
+
+
+
