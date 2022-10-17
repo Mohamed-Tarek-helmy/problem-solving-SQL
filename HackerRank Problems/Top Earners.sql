0ca067721e24cb80a5d5c54d489fe9bbf436bf7d
Mohamed-Tarek-helmy <115006636+Mohamed-Tarek-helmy@users.noreply.github.com>
2022-10-17 12:29:08 +0200

    Create Top Earners.sql

---
+++ HackerRank Problems/Top Earners.sql
@@ -0,0 +1,17 @@
+/*
+link     :https://www.hackerrank.com/challenges/earnings-of-employees/problem
+author   :Mohamed Tarek
+
+*/
+
+
+select top 1 (salary * months),
+count (salary * months) from Employee 
+group by salary * months
+order by salary * months desc
+
+
+/* another solution */
+
+select max(salary * months),count(salary * months) from Employee  
+where (salary * months) = 108064  from Employee)
