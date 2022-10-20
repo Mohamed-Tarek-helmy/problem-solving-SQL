f0ffab9b2863fd83d0449ac320cc274e8ec89da1
Mohamed-Tarek-helmy <115006636+Mohamed-Tarek-helmy@users.noreply.github.com>
2022-10-20 12:10:39 +0200

    Create The Blunder.sql

---
+++ HackerRank Problems/The Blunder.sql
@@ -0,0 +1,9 @@
+/*
+link     :https://www.hackerrank.com/challenges/weather-observation-station-14/problem
+
+author   :Mohamed Tarek
+
+note: replace used to replace integer and srings  thanks to Mohamed Ibrahim MY BEST FRIEND.
+*/
+
+select ceil (avg(salary) - avg(replace(salary,'0',''))) from employees;
